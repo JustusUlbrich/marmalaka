e425da573be7dcf5fabf58ac568c22c227f026ab
Justus Ulbrich <justus.ulbrich@student.kit.edu>
2015-01-23 22:39:28 +0100

    test level for controls added

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject m_levelPlane;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
