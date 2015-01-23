cd7216f1cdc7511ced5bf6893fee90515e00d0e0
Justus Ulbrich <justus.ulbrich@student.kit.edu>
2015-01-23 23:48:55 +0100

    Stub for level generator and input controller

---
+++ Assets/InputController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum PlayerActionType {Forward, TurnLeft, TurnRight, TurnBack, Jump, Attack};
+
+public class PlayerAction {
+    float timeInSlot;
+    PlayerActionType actionType;
+}
+
+public class InputController : MonoBehaviour {
+    ArrayList actions;
+
+	// Use this for initialization
+	void Start () {
+        PlayerAction action = new PlayerAction();
+        PlayerActionType type = PlayerActionType.Forward;
+
+        actions.Add(action);
+        actions.Add(2);
+        actions.Add(type);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void checkKeyboardInput() {
+        Input.GetButtonDown("P1Forward");
+    }
+}
---
+++ Assets/LevelPrefabs/LevelGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+    public GameObject levelBaseCube;
+    public int levelSize;
+
+	// Use this for initialization
+	void Start () {
+        for (int x = -levelSize / 2; x < levelSize / 2; x++)
+        {
+            for (int z = -levelSize / 2; z < levelSize / 2; z++)
+            {
+                GameObject.Instantiate(levelBaseCube, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        } 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
