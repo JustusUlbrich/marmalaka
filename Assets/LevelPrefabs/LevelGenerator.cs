e3a1416ec15d3125174dc9ec1881cdc9a4e4e3c5
Jens Krug <j.krug611@googlemail.com>
2015-01-24 04:13:06 +0100

    add weighted random function for map generation

--- Assets/LevelPrefabs/LevelGenerator.cs
+++ Assets/LevelPrefabs/LevelGenerator.cs
@@ -16,32 +16,71 @@ public class LevelGenerator : MonoBehaviour
     public List<float> topPropability;
 
     // Use this for initialization
+
     void Start()
     {
-
+        List<List<float>> objectDensities = new List<List<float>>();
+        objectDensities.Add(computeDensity(floorPropability));
+        objectDensities.Add(computeDensity(middlePropability));
+        objectDensities.Add(computeDensity(topPropability));
+        
         Random.seed = seed;
         List<List<GameObject>> objectTypes = new List<List<GameObject>>();
         objectTypes.Add(floor);
         objectTypes.Add(middle);
         objectTypes.Add(top);
+        Random.Range(0f,1f);
         levelGrid3d = new List<List<List<GameObject>>>();
         
         List<GameObject> currentObjects = new List<GameObject>();
+        List<float> curDensity = new List<float>();
         for (int y = 0; y <= 2; y++)
         {
+            curDensity.Clear();
+            curDensity.AddRange(objectDensities[y]);
+
             currentObjects.Clear();
             levelGrid3d.Add(new List<List<GameObject>>());
             currentObjects.AddRange(objectTypes[y]);
-            Random.Range(-0.499999999f, currentObjects.Count - 0.50000001f);
+
             for (int x = 0; x < levelSize; x++)
             {
                 levelGrid3d[y].Add(new List<GameObject>());
                 for (int z = 0; z < levelSize; z++)
                 {
-                    levelGrid3d[y][x].Add((GameObject)GameObject.Instantiate(currentObjects[(int)Mathf.Round(Random.value)], new Vector3(x, y, z), Quaternion.identity));
+                    float randomNumber = Random.value;
+                    for(int i = 0; i < curDensity.Count; i++)
+                    {
+                        levelGrid3d[y][x].Add(null);
+                        if(randomNumber < curDensity[i])
+                        {
+                            if(y > 0) {
+                                if(levelGrid3d[y-1][x][z] != null) {
+                                    levelGrid3d[y][x][z] = ((GameObject)GameObject.Instantiate(currentObjects[i], new Vector3(x, y, z), Quaternion.identity));
+                                    break;
+                                }
+                            } else 
+                            {
+                                levelGrid3d[y][x][z] = ((GameObject)GameObject.Instantiate(currentObjects[i], new Vector3(x, y, z), Quaternion.identity));
+                            }
+
+                        }
+                    }
                 }
             }
         }
+    }
+
+    List<float> computeDensity(List<float> probabilityList)
+    {
+        List<float> densityList = new List<float>();
+        float sum = 0;
+        for (int i = 0; i < probabilityList.Count; i++)
+        {
+            sum += probabilityList[i];
+            densityList.Add(sum);
+        }
+        return densityList;
     }
   
     // Update is called once per frame
