827b9b98c8b1132bd3dcf89ee296ba8fe24de1e4
Jens Krug <j.krug611@googlemail.com>
2015-01-24 02:34:28 +0100

    add simple level producer

--- Assets/LevelPrefabs/LevelGenerator.cs
+++ Assets/LevelPrefabs/LevelGenerator.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class LevelGenerator : MonoBehaviour {
-    public GameObject levelBaseCube;
+public class LevelGenerator : MonoBehaviour
+{
+    public List<GameObject> floor;
+    public List<GameObject> middle;
+    public List<GameObject> top;
+    public List<List<List<GameObject>>> levelGrid3d;
     public int levelSize;
+    public int seed;
 
-	// Use this for initialization
-	void Start () {
-        for (int x = -levelSize / 2; x < levelSize / 2; x++)
+    // Use this for initialization
+    void Start()
+    {
+
+        Random.seed = seed;
+        List<List<GameObject>> objectTypes = new List<List<GameObject>>();
+        objectTypes.Add(floor);
+        objectTypes.Add(middle);
+        objectTypes.Add(top);
+        levelGrid3d = new List<List<List<GameObject>>>();
+        
+        List<GameObject> currentObjects = new List<GameObject>();
+        for (int y = 0; y <= 2; y++)
         {
-            for (int z = -levelSize / 2; z < levelSize / 2; z++)
+            currentObjects.Clear();
+            levelGrid3d.Add(new List<List<GameObject>>());
+            currentObjects.AddRange(objectTypes[y]);
+            Random.Range(-0.499999999f, currentObjects.Count - 0.50000001f);
+            for (int x = 0; x < levelSize; x++)
             {
-                GameObject.Instantiate(levelBaseCube, new Vector3(x, 0, z), Quaternion.identity);
+                levelGrid3d[y].Add(new List<GameObject>());
+                for (int z = 0; z < levelSize; z++)
+                {
+                    levelGrid3d[y][x].Add((GameObject)GameObject.Instantiate(currentObjects[(int)Mathf.Round(Random.value)], new Vector3(x, y, z), Quaternion.identity));
+                }
             }
-        } 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        }
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
 }
