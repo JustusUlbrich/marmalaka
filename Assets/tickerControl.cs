eff0d1e9f3f260e91ed7025549564a3a50c29cdb
DasPogoton <daspogoton@gmail.com>
2015-01-25 02:49:02 +0100

    Merge conflicts, ya know?

--- Assets/tickerControl.cs
+++ Assets/tickerControl.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class tickerControl : MonoBehaviour {
+public class tickerControl : MonoBehaviour
+{
     float maxSlotTime;
 
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initialization
+    void Start ()
+    {
+    }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update ()
+    {
 
         maxSlotTime = TurnTimer.singleton.moveTime * TurnTimer.singleton.numberOfMoves;
 
-        float timeInSlot = TurnTimer.getTimerData().timeInTurn;
+        float timeInSlot = TurnTimer.getInputTimerData ().timeInTurn;
         float angle = -360 * (timeInSlot / maxSlotTime);
 
-        this.transform.localRotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
-	}
+        this.transform.localRotation = Quaternion.AngleAxis (angle, new Vector3 (0, 0, 1));
+    }
 }
