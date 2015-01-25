40d3f2cbc6f1b8a0c394be616e63c0361c168c5c
dre04 <andreak0404@gmail.com>
2015-01-25 02:05:40 +0100

    Character actions implementiert. Hoffe dit passt so, konnts nicht ausprobieren.

--- Assets/Scenes/Controller Test/Character.cs
+++ Assets/Scenes/Controller Test/Character.cs
@@ -4,11 +4,17 @@ using System.Collections;
 public class Character : MonoBehaviour {
 
 	public GameObject characterGO;
-	public Vector2 viewingDirection;
+	public int viewingDirectionIndex;
+	public Vector3 viewingDirection;
+	public const static Vector3[] VIEWING_DIRECTIONS = new Array[] {new Vector3(1, 0, 0), 
+		new Vector3(0, 0, 1), 
+		new Vector3(-1, 0, 0), 
+		new Vector3(0, 0, -1)};
 
 	// Use this for initialization
 	void Start () {
-		viewingDirection = (1, 0);
+		viewingDirectionIndex = 0;
+		viewingDirection = VIEWING_DIRECTIONS[0];
 	}
 	
 	// Update is called once per frame
@@ -46,26 +52,46 @@ public class Character : MonoBehaviour {
 	}
 
 	private void moveForward ()	{
-		Vector3 position = getPosition ();
-
+		Vector3 position = getPosition();
+		position += VIEWING_DIRECTIONS[viewingDirectionIndex];
+		characterGO.transform.position = position;
 	}
 
 	private void turnLeft() {
-		Vector3 position = getPosition ();
+		Vector3 position = getPosition();
+		viewingDirectionIndex ++;
+		if(viewingDirectionIndex >= VIEWING_DIRECTIONS.Length) {
+			viewingDirectionIndex = 0;
+		}
+		position += VIEWING_DIRECTIONS[viewingDirectionIndex];
+		characterGO.transform.LookAt(position);
 	}
 
 	private void turnRight() {
+		Vector3 position = getPosition();
+		viewingDirectionIndex --;
+		if(viewingDirectionIndex < 0) {
+			viewingDirectionIndex = VIEWING_DIRECTIONS.Length - 1;
+		}
+		position += VIEWING_DIRECTIONS[viewingDirectionIndex];
+		characterGO.transform.LookAt(position);
 	}
 
 	private void turnBack() {
+		Vector3 position = getPosition();
+		viewingDirection.x *= -1;
+		viewingDirection.y *= -1;
+		position += VIEWING_DIRECTIONS[viewingDirectionIndex];
+		characterGO.transform.LookAt(position);
 	}
 
 	//Jump up and down and move it all around *lalala
 	private void jump() {
+		//TODO
 	}
 
 	private void attack () {
-		throw new System.NotImplementedException ();
+		//TODO
 	}
 
 	private Vector3 getPosition ()
