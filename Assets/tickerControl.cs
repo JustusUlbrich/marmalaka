4d74e7e8154dcf639c9bbe2b5d5a174788f12ae3
Justus Ulbrich <justus.ulbrich@student.kit.edu>
2015-01-25 01:44:20 +0100

    Added models and ticker controler

---
+++ Assets/tickerControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tickerControl : MonoBehaviour {
+    float maxSlotTime;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        maxSlotTime = SlotTimerScript.singleton.maxSlotTime;
+
+        float timeInSlot  = SlotTimerScript.getTimerData().timeInSlot;
+        float angle = -360 * (timeInSlot / maxSlotTime);
+
+        this.transform.localRotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        //this.transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+	}
+}
